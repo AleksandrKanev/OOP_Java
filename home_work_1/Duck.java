43c4db8386a3fb0529403a5f52b4e2c95a817cb0
Kanev Aleksandr <kanev085@yandex.ru>
2023-03-07 17:02:46 +0300

    проба

---
+++ home_work_1/Duck.java
@@ -0,0 +1,14 @@
+public class Duck extends Animal {
+    public Duck(String color, int legsCount) {
+        super(color, legsCount);
+    }
+
+    @Override
+    public void speak() {
+        System.out.printf("%s сказал: Кря!%n", getType());
+    }
+
+    public void fly() {
+        System.out.printf("%s полетел!%n", getType());
+    }
+}
